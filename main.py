0042d922fac380b27eabda929ad1e396c63d0b95
Ernad Husremovic <hernad@bring.out.ba>
2024-09-15 14:32:44 +0200

    init py

---
+++ main.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    name = "herad"
+    return {"hello": name}
+
+
+class Invoice(BaseModel):
+    id: int
+    name: str
+
+@app.post("/invoice")
+async def invoice(invoice_data: Invoice):
+    id = invoice_data.id
+    name = invoice_data.name
+    return {
+        "msg": "SVE_JE_OK",
+        "id": id,
+        "name": name,
+    }
+ 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, access_log=False, workers=2)
