0ca2bf5a407473288a85e3efda586c3446f0a369
Ernad Husremovic <hernad@bring.out.ba>
2024-09-15 16:38:43 +0200

    /api/invoices init

--- main.py
+++ main.py
@@ -9,19 +9,37 @@ def root():
     name = "hernad"
     return {"hello": name}
 
+# CURL *curl;
+#CURLcode res;
+#curl = curl_easy_init();
+#if(curl) {
+#  curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+#  curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:3566/api/invoices");
+#  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+#  curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
+#  struct curl_slist *headers = NULL;
+#  headers = curl_slist_append(headers, "Authorization: Bearer 0123456789abcdef0123456789abcdef");
+#  headers = curl_slist_append(headers, "RequestId: 12345");
+#  headers = curl_slist_append(headers, "Content-Type: application/json");
+#  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+#  const char *data = "{\n    \"invoiceRequest\": {\n        \"invoiceType\": \"Normal\",\n        \"transactionType\": \"Sale\",\n        \"payment\": [\n            {\n                \"amount\": 100.00,\n                \"paymentType\": \"Cash\"\n            }\n        ],\n        \"items\": [\n            {\n                \"name\": \"Artikl 1\",\n                \"labels\": [\n                    \"F\"\n                ],\n                \"totalAmount\": 100.00,\n                \"unitPrice\": 50.00,\n                \"quantity\": 2.000\n            }\n        ],\n        \"cashier\": \"Radnik 1\"\n    }\n}";
+#  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+#  res = curl_easy_perform(curl);
+#}
+#curl_easy_cleanup(curl);
 
 class Invoice(BaseModel):
-    id: int
-    name: str
+    invoiceRequest: int
+    invoiceType: str
 
-@app.post("/invoice")
+@app.post("/api/invoices")
 async def invoice(invoice_data: Invoice):
-    id = invoice_data.id
-    name = invoice_data.name
+    request = invoice_data.invoiceRequest
+    type = invoice_data.invoiceType
     return {
         "msg": "SVE_JE_OK",
-        "id": id,
-        "name": name,
+        "invoiceRequest": request,
+        "invoiceType": type,
     }
  
 
