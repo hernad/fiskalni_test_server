6ec88518256c7881d2d433114dd458e4da5f3115
Ernad Husremovic <hernad@bring.out.ba>
2024-09-24 09:48:11 +0200

    fix

---
+++ scripts/send_get_invoice.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl --location 'http://127.0.0.1:3566/api/invoices/RX4F7Y5L-RX4F7Y5L-138?receiptLayout=Slip&imageFormat=Png&includeHeaderAndFooter=true' \
+  --header 'Authorization: Bearer 0123456789abcdef0123456789abcdef'
